22c871f165b06e2d1c5ae82ece976cccd7d87864
Shreya Kadam <shreyakadamhces@gmail.com>
2025-05-24 19:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const selectField = document.getElementById('selectField');
+const selectText = document.getElementById('selectText');
+const list = document.getElementById('list');
+const options = document.querySelectorAll('.options');
+
+selectField.addEventListener('click', () => {
+  list.classList.toggle('show');
+  if (list.classList.contains('show')) {
+    selectText.textContent = 'Select City';
+  }
+});
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    selectText.textContent = option.textContent;
+    list.classList.remove('show'); // Close the list after selecting an option
+  });
+});
